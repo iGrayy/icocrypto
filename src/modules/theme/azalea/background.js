d5d27991a5919a9f6d141428e43493bc3f649b38
hieupth <64821726+hieupth@users.noreply.github.com>
2023-06-30 17:03:27 +0700

    chore(azalea): add background

---
+++ src/modules/theme/azalea/background.js
@@ -0,0 +1,10 @@
+/**
+ * Show background
+ */
+export function Background() {
+    return (
+        <div className='nk-ovm nk-ovm-repeat nk-ovm-fixed shape-i'>
+			<div className='ovm-line'/>
+		</div>
+    );
+};
