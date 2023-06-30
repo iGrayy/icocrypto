e8808721a91bf1db4feeb92c8db6875cc1345995
hieupth <64821726+hieupth@users.noreply.github.com>
2023-06-30 21:00:44 +0700

    chore(azalea): home banner

--- src/app/home/banner.js
+++ src/app/home/banner.js
@@ -9,6 +9,7 @@ import {
     ProgressPoint
 } from "@/src/modules/theme/azalea/processbar";
 import { Countdown } from "@/src/modules/theme/azalea/countdown";
+import { Button, EmButton } from "@/src/modules/common/buttons";
 
 
 export default function HomeBanner() {
@@ -17,7 +18,8 @@ export default function HomeBanner() {
             <BannerHead
                 title='WORLD FIRST DECENTRALIZED MARKETPLACE WITH BLOCKCHAIN INFRASTRUCTURE'
                 caption='The first decentralized Marketplace which makes simplifies and standarizes data with blockchain technology. We provides user-friendly, efficient and secure crypto solutions and utilizing blockchain technology.'
-                btn1={<a className="btn btn-primary btn-round">PRIVATE SALE</a>}
+                btn1={<Button classes='btn-primary' isRound={true}>PRIVATE SALE</Button>}
+                btn2={<Button classes='btn-primary' isRound={true} isFill={false}>TOKEN SALE</Button>}
             ></BannerHead>
             <BannerBottom
                 left={
@@ -44,6 +46,27 @@ export default function HomeBanner() {
                     ></Countdown>
                 }
             ></BannerBottom>
+            <BannerFooter
+                buttons={[
+                    <EmButton
+                        href='https://www.youtube.com/watch?v=SSo_EIwHSd4'
+                        classes='link link-light link-break video-popup'
+                        icon='icon-circle icon-border icon-animation fas fa-play'
+                    >How it works</EmButton>,
+                    <EmButton
+                        classes='link link-light link-break'
+                        icon='icon-circle icon-border fas fa-lightbulb'
+                    >Why buy Tokens now?</EmButton>
+                ]}
+                socials={[
+                    <EmButton icon='fab fa-twitter'></EmButton>,
+                    <EmButton icon='fab fa-medium'></EmButton>,
+                    <EmButton icon='fab fa-facebook-f'></EmButton>,
+                    <EmButton icon='fab fa-youtube'></EmButton>,
+                    <EmButton icon='fab fa-bitcoin'></EmButton>,
+                    <EmButton icon='fab fa-github'></EmButton>
+                ]}
+            ></BannerFooter>
         </Banner>
     );
 };
