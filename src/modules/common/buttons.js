577dc686856cec935d7010ce3f62b30a29ead19b
hieupth <64821726+hieupth@users.noreply.github.com>
2023-06-30 20:54:52 +0700

    chore: add em button

--- src/modules/common/buttons.js
+++ src/modules/common/buttons.js
@@ -1,16 +1,35 @@
 
 /**
  * Show common link button.
- * @param {string} href Reference of button.
+ * @param {string} href Reference link.
  * @param {string} classes Custom classes.
  * @param {boolean} isFill Is button filled.
  * @param {boolean} isRound Is button rounded.
+ * @param {*} children Children elements.
  * @returns 
  */
-export function Button({children, href, classes, isFill=true, isRound=true}) {
+export function Button({children, href='#', classes, isFill=true, isRound=true}) {
     return (
         <a href={href} className={`btn ${classes} ${isFill ? '' : 'btn-outline'} ${isRound ? 'btn-round' : ''}`}>
             {children}
+        </a>
+    );
+};
+
+
+/**
+ * Show em button.
+ * @param {string} href Reference link.
+ * @param {string} classes Custom classes.
+ * @param {string} icon Custom icon.
+ * @param {*} children Children elements.
+ * @returns 
+ */
+export function EmButton({children, href='#', classes, icon}) {
+    return (
+        <a href={href} className={`link ${classes}`}>
+            <em className={`${icon}`}></em>
+            <span>{children}</span>
         </a>
     );
 };
